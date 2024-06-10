7ccc519235a1c2bea3de7e066f82ebd2dc4cee5a
qqbn <jakubkroc23@gmail.com>
2024-06-10 21:18:24 +0200

    frontend - adding new expense

--- src/components/expanses/AddExpense.vue
+++ src/components/expanses/AddExpense.vue
@@ -1,7 +1,12 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
+import axios from 'axios';
+import { apiUrl } from '@/helpers/constants';
+import { useExpensesStore } from '@/stores/expenses';
+
+const store = useExpensesStore();
 const dialog = ref<boolean>(false);
-const date = null;
+const date = ref<any>('');
 const typeItems = [
     { id: 1, name: 'Bills' },
     { id: 2, name: 'Food' },
@@ -10,6 +15,22 @@ const typeItems = [
     { id: 5, name: 'Others' }
 ]
 const selectedType = ref<[] | null>(null);
+const title = ref<string>('');
+const value = ref<number>(0);
+
+const handleAddNote = async (): Promise<void> => {
+    try {
+        const response = await axios.post(apiUrl + `expenses/addExpense/`, { title: title.value, value: Number(value.value), type: selectedType.value, date: date.value });
+        if (response.status === 200) {
+            console.log(response.data);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    finally {
+        dialog.value = false;
+    }
+}
 </script>
 <template>
     <v-btn color="primary" @click="dialog = true">
@@ -22,15 +43,15 @@ const selectedType = ref<[] | null>(null);
                 Add new expense
             </v-card-title>
             <v-card-text>
-                <v-text-field label="Expense Title" type="input"></v-text-field>
-                <v-text-field label="Expense Value" type="number"></v-text-field>
+                <v-text-field label="Expense Title" type="input" v-model="title"></v-text-field>
+                <v-text-field label="Expense Value" type="number" v-model="value"></v-text-field>
                 <v-select label="Type of expense" :items="typeItems" v-model="selectedType" item-title="name"
                     item-value="id"></v-select>
-                <VueDatePicker v-model="date" inline auto-apply></VueDatePicker>
+                <VueDatePicker v-model="date" inline auto-apply :min-date="new Date()"></VueDatePicker>
             </v-card-text>
             <v-card-actions class="d-flex justify-end align-center pa-4" align="center" justify="end">
                 <v-btn variant="tonal" color="red" @click="dialog = false">Close</v-btn>
-                <v-btn variant="tonal" color="primary" @click="dialog = false" type="button">Add Expense</v-btn>
+                <v-btn variant="tonal" color="primary" @click="handleAddNote" type="button">Add Expense</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
